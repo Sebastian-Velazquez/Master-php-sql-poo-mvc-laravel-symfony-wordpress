a59e2a05ae4f3554bb682e4c1600295de61d492b
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-09-15 19:51:17 -0300

    trabajando en las sentecias de sql para para pasar a mano la BD mysql y que funcione con auth laravel 10

--- laravel/database/database.sql
+++ laravel/database/database.sql
@@ -30,7 +30,7 @@ CREATE TABLE `users` (
 
 --Juntadndo tablas
 ALTER TABLE `users`
-  ADD `role`            varchar(20),
+  ADD role            varchar(20),
   ADD `surname`         varchar(200),
   ADD `nick`            varchar(100),
   ADD `image`           varchar(255);
@@ -41,20 +41,21 @@ ALTER TABLE `users`
   --ENGINE=InnoDb;
 
 ---Insertando datos 
-INSERT INTO users VALUES(NULL, 'user', 'Víctor', 'Robles', 'victorroblesweb', 'victor@victor.com', 'pass', null, CURTIME(), CURTIME(), NULL);
-INSERT INTO users VALUES(NULL, 'user', 'Juan', 'Lopez', 'juanlopez', 'juan@juan.com', 'pass', null, CURTIME(), CURTIME(), NULL);
-INSERT INTO users VALUES(NULL, 'user', 'Manolo', 'Garcia', 'manologarcia', 'manolo@manolo.com', 'pass', null, CURTIME(), CURTIME(), NULL);
+--INSERT INTO users VALUES(NULL, 'user', 'Víctor', 'Robles', 'victorroblesweb', 'victor@victor.com', 'pass', null, CURTIME(), CURTIME(), NULL);
+--INSERT INTO users VALUES(NULL, 'user', 'Juan', 'Lopez', 'juanlopez', 'juan@juan.com', 'pass', null, CURTIME(), CURTIME(), NULL);
+--INSERT INTO users VALUES(NULL, 'user', 'Manolo', 'Garcia', 'manologarcia', 'manolo@manolo.com', 'pass', null, CURTIME(), CURTIME(), NULL);
 
-CREATE TABLE IF NOT EXISTS images(
-id              int(255) auto_increment not null,
-user_id         int(255),
-image_path      varchar(255),
-description     text,
-created_at      datetime,
-updated_at      datetime,
-CONSTRAINT pk_images PRIMARY KEY(id),
-CONSTRAINT fk_images_users FOREIGN KEY(user_id) REFERENCES users(id)
-)ENGINE=InnoDb;
+CREATE TABLE IF NOT EXISTS images (
+    id int(255) auto_increment not null,
+    user_id bigint(20) unsigned,
+    image_path varchar(255),
+    description text,
+    created_at datetime,
+    updated_at datetime,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+) ENGINE=InnoDB;
+
 
 INSERT INTO images VALUES(NULL, 1, 'test.jpg', 'descripción de prueba 1', CURTIME(), CURTIME());
 INSERT INTO images VALUES(NULL, 1, 'playa.jpg', 'descripción de prueba 2', CURTIME(), CURTIME());
@@ -64,7 +65,7 @@ INSERT INTO images VALUES(NULL, 3, 'familia.jpg', 'descripción de prueba 4', CU
 
 CREATE TABLE IF NOT EXISTS comments(
 id              int(255) auto_increment not null,
-user_id         int(255),
+user_id         bigint(20) unsigned,
 image_id        int(255),
 content         text,
 created_at      datetime,
@@ -80,7 +81,7 @@ INSERT INTO comments VALUES(NULL, 2, 4, 'que bueno!!', CURTIME(), CURTIME());
 
 CREATE TABLE IF NOT EXISTS likes(
 id              int(255) auto_increment not null,
-user_id         int(255),
+user_id         bigint(20) unsigned,
 image_id        int(255),
 created_at      datetime,
 updated_at      datetime,
